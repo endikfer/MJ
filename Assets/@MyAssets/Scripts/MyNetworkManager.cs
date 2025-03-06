60b2dff0e657cb8f0482b2794bb48a93c4ab27d9
HugoReyIn <hugo.rey@opendeusto.es>
2025-03-06 16:04:07 +0100

    descargas

---
+++ Assets/@MyAssets/Scripts/MyNetworkManager.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MyNetworkManager : MonoBehaviour
+{
+    private const int MAX_PLAYER_AMOUNT = 2;
+
+    public void StartHost()
+    {
+       
+    }
+}
