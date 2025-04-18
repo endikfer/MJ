13c1f5244d6e57c3707e20cd0625ccbc5dc184be
Endika Fernandez <fernandez.endika@opendeusto.es>
2025-04-18 18:19:53 +0200

    cambios en rl

---
+++ RL/Assets/Free Wood Door Pack/Script/Door.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door : MonoBehaviour {
+	public bool open;
+	public float smooth = 1.0f;
+	float DoorOpenAngle = -90.0f;
+    float DoorCloseAngle = 0.0f;
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (open)
+		{
+            var target = Quaternion.Euler (0, DoorOpenAngle, 0);
+            transform.localRotation = Quaternion.Slerp(transform.localRotation, target, Time.deltaTime * 5 * smooth);
+	
+		}
+		else
+		{
+            var target1= Quaternion.Euler (0, DoorCloseAngle, 0);
+            transform.localRotation = Quaternion.Slerp(transform.localRotation, target1, Time.deltaTime * 5 * smooth);
+	
+		}  
+	}
+
+	public void OpenDoor(){
+		open =!open;
+	}
+}
