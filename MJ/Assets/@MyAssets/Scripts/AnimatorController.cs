b9736018aa4a6009521fb337689fe9a04862d8e8
HugoReyIn <hugo.rey@opendeusto.es>
2025-05-21 20:29:06 +0200

    corregido el network animator

--- MJ/Assets/@MyAssets/Scripts/AnimatorController.cs
+++ MJ/Assets/@MyAssets/Scripts/AnimatorController.cs
@@ -9,32 +9,17 @@ public class AnimatorController : NetworkBehaviour
     private Animator animator;
     private CharacterController xrCharacterController;
 
-    public string xrOriginName = "XR Origin"; // asegúrate de que se llame así
+    public string xrOriginName = "XR Origin";
     private NetworkAnimator networkAnimator;
 
     public bool isDead = false;
 
     public override void OnNetworkSpawn()
     {
-        if (!IsOwner) return; // solo controla su propio avatar
+        if (!IsOwner) return;
 
         animator = GetComponent<Animator>();
         networkAnimator = GetComponent<NetworkAnimator>();
-
-        // VERIFICACIONES EXTRA
-        if (animator == null)
-        {
-            Debug.LogError("Animator no encontrado en el jugador.");
-        }
-        else if (animator.runtimeAnimatorController == null)
-        {
-            Debug.LogError("Animator Controller no asignado al Animator.");
-        }
-
-        if (networkAnimator == null)
-        {
-            Debug.LogWarning("NetworkAnimator no encontrado en el jugador.");
-        }
 
         // Intenta buscar el XR Origin del jugador local
         GameObject xrOrigin = GameObject.Find(xrOriginName);
@@ -42,13 +27,7 @@ public class AnimatorController : NetworkBehaviour
         {
             xrCharacterController = xrOrigin.GetComponent<CharacterController>();
         }
-
-        if (xrCharacterController == null)
-        {
-            Debug.LogWarning("CharacterController no encontrado en XR Origin.");
-        }
     }
-
 
     void Update()
     {
@@ -77,7 +56,6 @@ public class AnimatorController : NetworkBehaviour
     {
         if (IsOwner)
         {
-            // Llama al RPC para sincronizar el estado de "muerte"
             DieServerRpc();
         }
     }
@@ -86,8 +64,6 @@ public class AnimatorController : NetworkBehaviour
     public void DieServerRpc()
     {
         isDead = true;
-
-        // Sincroniza el estado en todos los clientes
         networkAnimator.SetTrigger("Die");
     }
 }
